501a5fa876e1cfc96a99a49e6e039e50b6ab0c08
xing-6464 <xing_6464@foxmail.com>
2022-12-06 22:14:19 +0800

    修改代码

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <header class="App-content">
+    <h1>Hello world</h1>
+  </header>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 })
 </script>
