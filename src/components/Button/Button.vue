648f2e4a1013c92b434a5560fa3e177d79b5c5bb
xing-6464 <xing_6464@foxmail.com>
2022-12-06 22:14:29 +0800

    Button组件

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,46 @@
+<template>
+  <a
+    v-if="(props.btnType === 'link' && props.href)"
+    :class="classes"
+    :href="props.href"
+  >
+  </a>
+  <button
+    v-else
+    :disabled="props.disabled"
+    :class="classes"
+  >
+  </button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+import classNames from 'classnames'
+
+type ButtonSize = 'lg' | 'sm'
+type ButtonType = 'primary' | 'default' | 'danger' | 'link'
+
+interface BaseButtonProps {
+  className?: string
+  href?: string
+  disabled?: boolean
+  size?: ButtonSize
+  btnType?: ButtonType
+}
+
+const props = withDefaults(defineProps<BaseButtonProps>(), {
+  btnType: 'default',
+  disabled: false
+})
+
+const classes = classNames('btn', {
+  [`btn-${props.btnType}`]: props.btnType,
+  [`btn-${props.size}`]: props.size,
+  disabled: (props.btnType === 'link') && props.disabled
+})
+
+</script>
+
+<style lang="scss">
+
+</style>
